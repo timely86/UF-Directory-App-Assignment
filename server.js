4acf7f74a6a7797f1e674a2757104d52f9ed10a3
Timothy Ely <tely@ufl.edu>
2018-09-03 20:33:09 -0400

    passed all 3 test cases, done with version.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var http = require('http'), 
-    fs = require('fs'), 
+var http = require('http'),
+    fs = require('fs'),
     url = require('url'),
     port = 8080;
 
@@ -9,18 +9,38 @@ var listingData, server;
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
 
-  /*
-    Your request handler should send listingData in the JSON format if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+  if(request.url == '/listings'){
+    response.writeHead(200);
+    response.write(listingData);
 
-    HINT: explore the request object and its properties 
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-   */
+    response.end();
+  }
+
+
+   else{
+     response.writeHead(404);
+     response.write('Bad gateway error');
+     response.end();
+   }
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
-   */
+  if(err){
+  console.log(err);
+  }
+  else{
+  listingData = data;
+  }
+
 });
+  /*
+    This callback function should save the data in the listingData variable,
+    then start the server.
+   */
+   var server = http.createServer(requestHandler);
+
+   // the server is now started, listening for requests on port 8080
+   server.listen(port, function() {
+     //once the server is listening, this callback function is executed
+     console.log('Server listening on: http://127.0.0.1:' + port);
+   });
